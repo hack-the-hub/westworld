87619cc648c97b14af34022fa185fc36ab8e7f6e
MichaelGrundie <mgrundie01@qub.ac.uk>
2018-03-24 18:54:12 +0000

    Remove jpgs

--- FeaturedDevelopers/FeaturedDevelopers.js
+++ FeaturedDevelopers/FeaturedDevelopers.js
@@ -5,7 +5,7 @@ export const featuredDevelopers = [
     'description': 'Andrew is a Systems Researcher with a wide range of skills and experiences across technical, business and entrepreneurial fields. He completed a PhD in autonomous systems at the University of Liverpool as part of an Anglo-French Defence Programme on Autonomous Submarines for Marine Survey and Monitoring. He is currently a Data Scientist at Sensum and the founder and director of Farset Labs – a Belfast based charity that is a hub for the tech community in Northern Ireland. ',
     'technology': 'Autonomous Systems',
     'role': 'Data Scientist',
-    'image': 'AndrewBolster.jpg'
+    'image': 'https://s1.postimg.org/55k87t1o5b/Andrew_Bolster_Image.jpg'
   },
   {
     'id': 2,
@@ -21,7 +21,7 @@ export const featuredDevelopers = [
     'description': 'Gemma Crothers has over a decade’s experience in the IT industry in a variety of consultancy roles. As Tech Outreach Manager at Kainos, she heads up their education outreach programme, which is designed to inspire, enthuse and educate young people to secure the future of the digital economy.',
     'technology': 'Education',
     'role': 'Tech Outreach Manager',
-    'image': 'GemmaCrothers.jpg'
+    'image': 'https://s1.postimg.org/5dcyvks9hb/Gemma_Crothers_-_Image.jpg'
   },
   {
     'id': 4,
@@ -29,7 +29,7 @@ export const featuredDevelopers = [
     'description': 'Lisa is Youth Outreach Lead with Women Who Code Belfast (WWCB). She is currently supporting Business In The Community/Code Club on the ‘Time To Code’ initiative which involves 25 schools across Belfast & the North West. She will be Mentoring on the upcoming Tech Know Clubs school project which is a partnership between STEM UK, Allstate and Go Berserk! Lisa is also visiting with schools under the CAS Barefoot programme, which offers free support to Primary School teachers to encourage them to use tech in the current curriculum. She is also putting the finishing touches on a new blog site to help people (primarily mums and women) get involved with tech.',
     'technology': 'Education',
     'role': 'Youth Outreach Lead',
-    'image': 'LisaDonaldson.jpg'
+    'image': 'https://s1.postimg.org/43a1p9ezxb/Lisa_Donaldson_Image.jpg'
   },
   {
     'id': 5,
@@ -37,6 +37,6 @@ export const featuredDevelopers = [
     'description': 'Victoria McCallum is a diversity and inclusion advocate and works to support Women in Tech Belfast, and is the instigator of the Women in Tech Lean In circle; and course co-ordinator at Queen’s University Belfast for Code First: Girls.',
     'technology': 'Education',
     'role': 'Course co-ordinator at Queen’s University',
-    'image': 'VictoriaMcCallum.jpg'
+    'image': 'https://s1.postimg.org/18hdjh6vsf/vic.jpg'
   }
 ]
