9c881b3bf279a513692f0fe65720acc46e2217f8
Kyle Harrison <kyle90adam@hotmail.com>
2018-07-07 11:57:18 +0100

    Adding video ui to client

---
+++ services/client/src/components/video/content.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles/content";
+import TimeToVideo from "./time-to-video";
+
+const formatTitle = str =>
+  str.toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
+
+const Content = ({ name, created, url, channel }) => (
+  <React.Fragment>
+    <style jsx>{styles}</style>
+    <div className="video-content">
+      <a
+        className="video-title-link"
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {formatTitle(name)}
+      </a>
+    </div>
+    <div className="video-details">
+      <span>{channel}</span>
+      <TimeToVideo startTime={created}/>
+    </div>
+  </React.Fragment>
+);
+
+Content.propTypes = {
+  name: PropTypes.string.isRequired,
+  created: PropTypes.number.isRequired,
+  url: PropTypes.string.isRequired,
+  channel: PropTypes.string.isRequired
+};
+
+export default Content;
