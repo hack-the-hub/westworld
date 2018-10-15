1b51140962d043433b3bb529122d1b4924673d38
Kyle Harrison <kyle90adam@hotmail.com>
2018-10-15 12:02:49 +0100

    Adding developers backend

--- scripts/src/github_users.py
+++ scripts/src/github_users.py
@@ -1,47 +1,63 @@
-# import PyGithub
+import os
+import requests
+from math import ceil
+import json
+
 from github import Github
 
 DEFAULT_PAGE_SIZE = 100
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-LOCATION = os.getenv("LOCATION")
+LOCATION = os.getenv("LOCATION", "Belfast")
+DEVELOPERS_ENDPOINT = os.getenv("DEVELOPERS_ENDPOINT")
 
 g = Github(GITHUB_TOKEN, per_page=DEFAULT_PAGE_SIZE)
 
-users = g.search_users('', location='Belfast')
 
-total_users = users.totalCount
+def get_developers():
+    """Gets developers from the Farset labs google calendar."""
+    users = g.search_users("", location=LOCATION)
 
-number_of_pages = total_users / DEFAULT_PAGE_SIZE
+    total_users = users.totalCount
+    number_of_pages = total_users / DEFAULT_PAGE_SIZE
 
-page = [p for p in users.get_page(0)]
+    for page_number in range(0, ceil(number_of_pages)):
+        print(f"loading page {page_number}")
+        developers = [developer for developer in users.get_page(page_number)]
 
-avatar_url = page[0].avatar_url
-login = page[0].login
-name = page[0].name
-location = page[0].location
-id = page[0].id
-public_gists = page[0].public_gists
-public_repos = page[0].public_repos
-url = page[0].url
-followers = page[0].followers
-company = page[0].company
-blog = page[0].blog
-bio = page[0].bio
+        transformed_developers = [_transform_developer(e) for e in developers]
+        _post_payloads(transformed_developers)
 
-user = {
-    "avatar_url": avatar_url,
-    "login": login,
-    "name": name,
-    "location": location,
-    "id": id,
-    "public_gists": public_gists,
-    "public_repos": public_repos,
-    "url": url,
-    "followers": followers,
-    "company": company,
-    "blog": blog,
-    "bio": bio,
-}
 
-print(user)
+def _transform_developer(data):
+    developer = {
+        "avatar": data.avatar_url,
+        "login": data.login,
+        "name": data.name,
+        "location": data.location,
+        "gists": data.public_gists,
+        "repositories": data.public_repos,
+        "url": data.url,
+        "followers": data.followers,
+        "company": data.company if data.company else "",
+        "blog": data.blog if data.blog else "",
+        "bio": data.bio if data.bio else "",
+        "topics": [],
+        "source": "github",
+    }
+
+    return developer
+
+
+def _post_payloads(payloads):
+    for payload in payloads:
+        r = requests.post(
+            DEVELOPERS_ENDPOINT,
+            headers={"Content-type": "application/json"},
+            data=json.dumps(payload),
+        )
+        print(r.status_code)
+
+
+if __name__ == "__main__":
+    get_developers()
