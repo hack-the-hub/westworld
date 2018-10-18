08337edcb38ae305edc8fecf5214f850e4cad8a3
Warao Gil <wgil14@gmail.com>
2018-10-17 21:28:41 -0400

    Remove unused file.

--- services/client/src/selectors/index.js
+++
@@ -1,9 +0,0 @@
-export const getRecentEvents = state => state.events.recentEvents;
-
-export const getUpcomingEvents = state => state.events.upcomingEvents;
-
-export const isLoading = state => state.events.isLoading;
-
-export const hasErrors = state => state.events.hasErrors;
-
-export const hasMoreItems = state => state.events.hasMoreItems;
